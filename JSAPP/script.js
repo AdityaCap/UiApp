c11594dd76c789137fd9443c40fa52e856db8c78
Aditya Verma <aditya.verma@capgemini.com>
2023-02-03 10:47:41 +0530

    async await in fetch for all posts

--- JSAPP/script.js
+++ JSAPP/script.js
@@ -1,19 +1,18 @@
-function getAllPosts() {
+async function getAllPosts() {
     let posts = [];
-
-    fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
-        .then(data => {
-            data.forEach(p => {
-                posts.push(p);
-            });
-        });
-
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    let data = await response.json();
+    posts = data;
     /*
     1. Call the API
     2. convert response to JSON
     3. assign data of API to posts
     */
+    let str = '';
+    posts.forEach(p => {
+        str = str + '<div class="jumbotron"><h1 class="display-6">' +
+            p.title + '</h1><p class="lead">' + p.body + '</p><hr class="my-4"></div>';
+    });
+    document.getElementById('post').innerHTML = str;
 
-    console.log(posts);
 }
